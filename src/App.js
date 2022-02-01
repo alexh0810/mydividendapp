42b1592be397ffdf162a14ced5a831e42ba7cd6e
alexh0810 <baohoan081097@gmail.com>
2022-02-01 18:37:19 +0200

    Fixing second solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState }from 'react';
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState([]);
+  const url = "https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json";
+    const fetchData = () => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch(err => console.log(err));
+    }
+    useEffect(() => fetchData(), []);
+    console.log(data);
+
+    const dividend2021 = data.map(el => el.dividendHistory[0].dividend);
+    const dividendHistory = data.map(el => el.dividendHistory.map(x => x.dividend));
+    console.log(dividendHistory);
+    const dividendInFiveYears = dividendHistory.map(el => el.slice(0,5))
+    console.log(dividendInFiveYears);
+
+
+
+
+     const sumArr = [];
+     let sum = 0;
+
+     for (let i = 0; i < dividendInFiveYears.length; i++) {
+       for (let k = 0; i < dividendInFiveYears[i].length; k++) {
+         sum += dividendInFiveYears[i][k]
+         sumArr.push(sum);
+       }
+     }
+
+     console.log(sumArr);
+
+    
+    
+    const price = data.map(el => el.price);
+    // console.log(price);
+    // console.log(dividend2021);
+    const yieldArr = []; 
+    for (let i = 0; i < dividend2021.length; i++) {
+      let y = (dividend2021[i] / price[i] * 100).toFixed(2);
+      yieldArr.push(y);
+    }
+    console.log(yieldArr);
+    const averageDividend = [];
+
+    for (let i = 0; i < dividendInFiveYears.length; i++) {
+      let avg = ((sumArr[i]/5) / price[i] * 100).toFixed(2);
+      averageDividend.push(avg); 
+    }
+
+    console.log(averageDividend);
+
+    /*const weights = [3,2,1,1,1];
+    const weightedAvgDiv = [];
+
+    for (let i = 0; i < dividendInFiveYears.length; i++) {
+      let weightedAvg = (sum/weights[i] / price[i] * 100).toFixed(2)
+      weightedAvgDiv.push(weightedAvg);
+    }
+
+    console.log(weightedAvgDiv); 
+    */
+
+   
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
